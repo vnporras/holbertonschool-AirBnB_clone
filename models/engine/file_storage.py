23774afc34a047e1b8cfd3caaf0cc1410dfb3fc9
Vanessa Requejo Porras <vnporras1028@gmail.com>
2024-03-12 15:09:24 -0500

    update the code

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -27,6 +27,6 @@ class FileStorage:
     def reload(self):
         """Deserializes the JSON file to __objects"""
         if os.path.exists(self.__file_path):
-            with open(self.__file_path.json, 'r') as file:
+            with open(self.__file_path, 'r') as file:
                 obj_dict = json.load(file)
                 self.__objects = {key: eval(obj['__class__'])(**obj) for key, obj in obj_dict.items()}
