9950368a2372960c01e81125ff4ded9f6bb0123e
Vanessa Requejo Porras <vnporras1028@gmail.com>
2024-03-12 11:11:43 -0500

    update the code

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,18 @@ import datetime
 
 class BaseModel:
     """ Base Model Class """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Constructor Method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """ String Representation of the Object"""
