df56b27fc9f23560a028f8b5700da8ad59f7e103
Vanessa Requejo Porras <vnporras1028@gmail.com>
2024-03-12 11:03:19 -0500

    modified the code

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,32 @@
 #!/usr/bin/python3
+
+""" Base Model Module """
 import uuid
 import datetime
 
+
 class BaseModel:
-    def __init__(self, *args, **kwargs):
+    """ Base Model Class """
+    def __init__(self):
+        """ Constructor Method"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
 
-    
     def __str__(self):
+        """ String Representation of the Object"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     
     def save(self):
+        """Updates the updated_at attribute with the current datetime"""
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        print(self.__dict__)
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
-        self.__dict__.__setitem__('__class__', self.__class__.__name__)
-
-        return dict(self.__dict__)
-    
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+        """Returns a dictionary containing all keys/values of the instance"""
+        obj_dict = self.__dict__.copy()
+        
+        obj_dict['created_at'] = obj_dict['created_at'].isoformat()
+        obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
+        obj_dict['__class__'] = self.__class__.__name__  # Add '__class__' key
+        
+        return obj_dict
