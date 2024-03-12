3a189db643d8fceff7ae1ad5c91f9e3a4bebc3f4
Vanessa Requejo Porras <vnporras1028@gmail.com>
2024-03-12 10:11:06 -0500

    create the files, directories and first code file

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import uuid
+import datetime
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        print(self.__dict__)
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
+        self.__dict__.__setitem__('__class__', self.__class__.__name__)
+
+        return dict(self.__dict__)
+    
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
