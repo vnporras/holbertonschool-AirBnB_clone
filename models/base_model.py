c6d6d48b98bb8da1be8d0f40eecd8820fb83e718
vnporrras <vnporras1028@gmail.com>
2024-03-13 21:39:22 -0500

    modified the code

--- models/base_model.py
+++ models/base_model.py
@@ -18,8 +18,8 @@ class BaseModel:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             storage.new(self)
 
     def __str__(self):
