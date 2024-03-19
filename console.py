997404f0f6708753a5034d04844793c88d40fda3
Vanessa Requejo Porras <vnporras1028@gmail.com>
2024-03-19 13:32:31 -0500

    modified

--- console.py
+++ console.py
@@ -10,12 +10,11 @@ from models.base_model import BaseModel
 from models import storage
 from datetime import datetime
 from models.user import User
-""" 
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from models.review import Review """
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """ Simple command processor example. """
